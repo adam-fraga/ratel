867ccfe60675cd3d403aa4a374632217829a42da
Adm FRG <adam.fraga@hotmail.com>
2024-05-25 13:22:31 -0400

    Add progress bar and colorised terminal message

--- go.mod
+++ go.mod
@@ -3,16 +3,20 @@ module github.com/adam-fraga/ratel
 go 1.22.2
 
 require (
+	github.com/k0kubun/go-ansi v0.0.0-20180517002512-3bf9e2903213
 	github.com/schollz/progressbar/v3 v3.14.3
 	github.com/spf13/cobra v1.8.0
 	github.com/spf13/viper v1.18.2
 )
 
 require (
+	github.com/fatih/color v1.17.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
---
+++ handlers/project/createProject.go
@@ -0,0 +1,261 @@
+package project
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+
+	"github.com/adam-fraga/ratel/errors"
+	"github.com/adam-fraga/ratel/handlers/utils"
+	"github.com/adam-fraga/ratel/models/datatypes"
+	"github.com/schollz/progressbar/v3"
+)
+
+// Init the project creation process
+func InitProject(appName string) {
+	CreateProjectStructure(appName)
+	PopulateProjectFiles()
+}
+
+// Create the project structure based on the data/projectStruct.json file
+func CreateProjectStructure(appName string) error {
+	utils.PrintInfoMsg("Creating the project structure for application "+appName, "\n")
+	projectStruct, err := getProjectStructFromJsonFIle()
+	if err != nil {
+		fmt.Println("Error parsing the json folders")
+		fmt.Println(err.Error())
+		return err
+	}
+
+	for _, folder := range projectStruct {
+		err := CreateFolder(&folder)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+
+	utils.PrintSuccessMsg("\nProject structure", "created\n")
+	return nil
+}
+
+// Create a folder with the given permissions and create the files and subfolders inside it
+func CreateFolder(folder *datatypes.Folder) error {
+	utils.PrintInfoMsg(fmt.Sprintf("%s Folder with permissions 755...", folder.FolderName), "created")
+
+	if folder.FolderName != "root" {
+		err := os.Mkdir(folder.FolderName, os.FileMode(0755))
+		if err != nil {
+			return &errors.Error{
+				Type:       "Project Structure Error",
+				Origin:     "createFolder()",
+				FileOrigin: "handlers/project.go",
+				Msg:        err.Error() + fmt.Sprintf("Error creating %s folder", folder.FolderName)}
+		}
+	}
+
+	if len(folder.Files) > 0 {
+		for _, file := range folder.Files {
+			err := CreateFile(file)
+			if err != nil {
+				return &errors.Error{
+					Type:       "Project Structure Error",
+					Origin:     "createFolder()",
+					FileOrigin: "handlers/project.go",
+					Msg:        err.Error() + fmt.Sprintf("Error creating file %s in folder %s", file, folder.FolderName)}
+			}
+		}
+	}
+
+	if len(folder.SubFolders) > 0 {
+		for _, subFolder := range folder.SubFolders {
+			err := CreateFolder(&subFolder)
+			if err != nil {
+				return &errors.Error{
+					Type:       "Project Structure Error",
+					Origin:     "createFolder()",
+					FileOrigin: "handlers/project.go",
+					Msg:        err.Error() + fmt.Sprintf("Error creating subfolder %s in folder %s", subFolder.FolderName, folder.FolderName)}
+			}
+		}
+	}
+
+	return nil
+}
+
+// Create a file with the given permissions
+func CreateFile(fileDestination string) error {
+	file, err := os.Create(fileDestination)
+	if err != nil {
+		return &errors.Error{
+			Type:       "Project Structure Error",
+			Origin:     "createFolder()",
+			FileOrigin: "handlers/project.go",
+			Msg:        err.Error() + fmt.Sprintf("Error creating %s file\n", fileDestination)}
+	}
+
+	err2 := os.Chmod(fileDestination, os.FileMode(0777))
+
+	if err2 != nil {
+		return &errors.Error{
+			Type:       "Project Structure Error",
+			Origin:     "createFolder()",
+			FileOrigin: "handlers/project.go",
+			Msg:        err2.Error() + fmt.Sprintf("Error changing permissions of %s file\n", fileDestination)}
+	}
+
+	defer file.Close()
+	return nil
+}
+
+// Populate the project files with the data from the data/configs folder using goroutines
+func PopulateProjectFiles() {
+
+	files, err := GetFilesFromProject()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(files["src"]))
+
+	for i := range files["src"] {
+		bar := utils.SetProgressBar(files["dst"][i])
+		go processFile(&wg, files["src"], files["dst"], i, bar)
+	}
+
+	time.Sleep(1 * time.Second)
+	wg.Wait()
+}
+
+// Process the file copying the contents from the source file to the destination file
+func processFile(wg *sync.WaitGroup, srcFiles []string, dstFiles []string, i int, pb *progressbar.ProgressBar) error {
+	defer wg.Done()
+	srcFile, err := os.OpenFile(srcFiles[i], os.O_RDONLY|os.O_CREATE, 0666)
+	dstFile, err := os.OpenFile(dstFiles[i], os.O_WRONLY|os.O_CREATE, 0666)
+
+	if err != nil {
+		return &errors.Error{
+			Type:       "Project Structure Error",
+			Origin:     "processFile()",
+			FileOrigin: "handlers/project.go",
+			Msg:        err.Error() + fmt.Sprintf("Error opening file %s\n", srcFiles[i])}
+	}
+	defer srcFile.Close()
+	defer dstFile.Close()
+
+	buf := new(bytes.Buffer)       // Create a new bytes.Buffer to store the file's contents
+	_, err = io.Copy(buf, srcFile) // Copy file contents into the buffer
+	if err != nil {
+		return &errors.Error{
+			Type:       "Project Structure Error",
+			Origin:     "processFile()",
+			FileOrigin: "handlers/project.go",
+			Msg:        err.Error() + fmt.Sprintf("Error reading file %s\n", srcFiles[i])}
+	}
+
+	_, err2 := buf.WriteTo(dstFile) // Write the buffer to the destination file
+	if err2 != nil {
+		return &errors.Error{
+			Type:       "Project Structure Error",
+			Origin:     "processFile()",
+			FileOrigin: "handlers/project.go",
+			Msg:        err2.Error() + fmt.Sprintf("Error writing to file %s\n", dstFiles[i])}
+	}
+
+	pb.Finish()
+	fmt.Println()
+	utils.PrintSuccessMsg(dstFiles[i], "populated")
+
+	return nil
+}
+
+// Get the files from the project structure and return a map with the source and destination files
+func GetFilesFromProject() (map[string][]string, error) {
+	var fileName string
+	var srcFiles []string
+	var dstFiles []string
+	files := make(map[string][]string)
+
+	dataConfigFilePath, err := filepath.Abs("/home/afraga/Projects/ratel/data/configs/")
+
+	if err != nil {
+		return nil, &errors.Error{
+			Type:       "Project Structure Error",
+			Origin:     "getFilesFromProject()",
+			FileOrigin: "handlers/project.go",
+			Msg:        err.Error() + fmt.Sprintf("Error getting the absolute path of the data config file\n")}
+	}
+
+	fmt.Println("Populating the project files...")
+	projectStruct, err := getProjectStructFromJsonFIle()
+	if err != nil {
+		return nil, &errors.Error{
+			Type:       "Project Structure Error",
+			Origin:     "getFilesFromProject()",
+			FileOrigin: "handlers/project.go",
+			Msg:        err.Error() + fmt.Sprintf("Error getting the project structure from json file\n")}
+	}
+
+	for _, folder := range projectStruct {
+		for _, file := range folder.Files {
+			fileName = filepath.Base(file)
+			dstFiles = append(dstFiles, file)
+			joinPath := filepath.Join(dataConfigFilePath, fileName)
+			srcFiles = append(srcFiles, joinPath)
+		}
+
+		for _, subFolder := range folder.SubFolders {
+			for _, file := range subFolder.Files {
+				fileName = filepath.Base(file)
+				dstFiles = append(dstFiles, file)
+				joinPath := filepath.Join(dataConfigFilePath, fileName)
+				srcFiles = append(srcFiles, joinPath)
+			}
+		}
+	}
+
+	files["src"] = srcFiles
+	files["dst"] = dstFiles
+
+	return files, nil
+}
+
+// Parse the project structure from the data/projectStruct.json file and return a slice of Folder structs
+func getProjectStructFromJsonFIle() ([]datatypes.Folder, error) {
+	var folders []datatypes.Folder
+
+	projectStructureJsonFilePath, err := filepath.Abs("/home/afraga/Projects/ratel/data/projectStruct.json")
+
+	if err != nil {
+		return nil, &errors.Error{
+			Type:       "Project Structure Error",
+			Origin:     "parseJsonFolders()",
+			FileOrigin: "handlers/project.go",
+			Msg:        err.Error() + fmt.Sprintf("Error getting the absolute path of the json file\n")}
+	}
+
+	jsonFile, err := os.Open(projectStructureJsonFilePath)
+	if err != nil {
+		return nil, &errors.Error{
+			Type:       "Project Structure Error",
+			Origin:     "parseJsonFolders()",
+			FileOrigin: "handlers/project.go",
+			Msg:        err.Error() + fmt.Sprintf("Error opening the json file %s\n", projectStructureJsonFilePath)}
+	}
+	defer jsonFile.Close()
+
+	err = json.NewDecoder(jsonFile).Decode(&folders)
+	if err != nil {
+		return nil, &errors.Error{
+			Type:       "Project Structure Error",
+			Origin:     "parseJsonFolders()",
+			FileOrigin: "handlers/project.go",
+			Msg:        err.Error() + fmt.Sprintf("Error decoding the json file %s\n", projectStructureJsonFilePath)}
+	}
+	return folders, nil
+}
